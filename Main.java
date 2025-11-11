42f5f3f7a570d02ea8cf542910ac5cc16e717727
youchan <youchan@localhost>
2025-11-11 16:56:19 +0900

    데이터 셋으로부터 레시피 상세 화면 구현

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
 import screen.MainDisplay;
-import utils.ThemeColor;
 
 public class Main {
     public static void main(String[] args) {
-        ThemeColor.applyThemeDark("utils/dark-ui.properties");
-        MainDisplay mainDisplay = new MainDisplay();
-        mainDisplay.run();
+        MainDisplay frame = new MainDisplay();
+        frame.setVisible(true);
     }
 }
