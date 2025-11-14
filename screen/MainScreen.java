1b6f60c25456d85a7253305edd75fe319d7e2468
kwj0175 <kwj075@gmail.com>
2025-11-14 22:15:09 +0900

    MOD: MainScreen 클래스명 변경, cardlayout 도입, 화면전환 메서드 추가

---
+++ screen/MainScreen.java
@@ -0,0 +1,84 @@
+package screen;
+
+import entity.User;
+import screen.home.HomeScreen;
+import screen.login.LoginScreen;
+import screen.planner.PlannerScreen;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainScreen extends JFrame {
+    public static final int DISPLAY_WIDTH = 360;
+    public static final int DISPLAY_HEIGHT = 640;
+
+    private final CardLayout cardLayout;
+    private final JPanel cardPanel;
+
+    private final HomeScreen homeScreen;
+    private final PlannerScreen plannerScreen;
+    private final LoginScreen loginScreen;
+
+    public MainScreen() {
+        setTitle("MySmartRecipe");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //일단 창닫으면 종료
+        setSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+        JPanel stage = new JPanel(new GridBagLayout());
+        setContentPane(stage);
+
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = c.gridy = 0;
+        add(cardPanel, c);
+
+        loginScreen = new LoginScreen(this);
+        homeScreen = new HomeScreen(this);
+        plannerScreen = new PlannerScreen();
+
+        cardPanel.add(loginScreen, "Login");
+        cardPanel.add(homeScreen, "Home");
+        cardPanel.add(plannerScreen, "Planner");
+
+        cardLayout.show(cardPanel, "Login");
+        setVisible(true);
+    }
+
+    public void run() {
+
+    }
+
+    public void displayHomeScreen(User loggedInUser) {
+        homeScreen.setCurrentUser(loggedInUser);
+        cardLayout.show(cardPanel, "Home");
+    }
+
+    public void displayPlannerScreen() {
+        cardLayout.show(cardPanel, "Planner");
+    }
+
+//    private void setBackgroundIMG(JLayeredPane layeredPane) {
+//        // --- 배경 이미지 로드 및 크기 조정 ---
+//        ImageIcon foodIcon = new ImageIcon(getClass().getResource("/HomeImage.jpg"));
+//        imageLabel = new JLabel();
+//        int frameWidth = 800;
+//        int frameHeight = 500;
+//
+//        if (foodIcon.getIconWidth() > 0) {
+//            Image scaledImage = foodIcon.getImage().getScaledInstance(frameWidth, frameHeight, Image.SCALE_SMOOTH);
+//            imageLabel.setIcon(new ImageIcon(scaledImage));
+//        } else {
+//            imageLabel.setText("이미지 로드 실패! /HomeImage.jpg 확인");
+//            imageLabel.setOpaque(true);
+//            imageLabel.setBackground(Color.WHITE);
+//            imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+//        }
+//
+//        imageLabel.setBounds(0, 0, frameWidth, frameHeight);
+//        layeredPane.add(imageLabel, JLayeredPane.DEFAULT_LAYER);
+//    }
+}
