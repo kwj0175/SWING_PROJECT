5457986f718286c4fe34df64efb4a37d39b672ae
김율희 <hwikysister3910@gmail.com>
2025-11-15 21:32:28 +0900

    Feat : 화면 구성 추가

--- screen/MenuCategoryDisplay.java
+++ screen/MenuCategoryDisplay.java
@@ -1,18 +1,8 @@
 package screen;
 import javax.swing.*;
-public class MenuCategoryDisplay {
+public class MenuCategoryDisplay extends JFrame {
     void display() {
-        JFrame frame = new JFrame("Menu Category Display");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(360, 640);
-        frame.setVisible(true);
-
-        frame.setLayout(null);
-
-        JLabel inputLabel = new JLabel("검색어 입력:");
-        // (x좌표, y좌표, 폭, 높이)
-        inputLabel.setBounds(20, 20, 100, 30);
-        frame.add(inputLabel);
+        
     }
     public static void main(String[] args) {
         MenuCategoryDisplay display = new MenuCategoryDisplay();
