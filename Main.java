d761613ab23a94fa9c647e78b553eed204538d0b
kwj0175 <kwj075@gmail.com>
2025-11-25 02:25:32 +0900

    MOD: Main 위치 변경

---
+++ Main.java
@@ -0,0 +1,14 @@
+import src.screen.MainScreen;
+import src.theme.ThemeColor;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ThemeColor.applyThemeDark("theme/dark-ui.properties");
+        SwingUtilities.invokeLater(() -> {
+            MainScreen mainScreen = new MainScreen();
+            mainScreen.run();
+        });
+    }
+}
